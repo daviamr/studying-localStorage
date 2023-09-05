17b9c2e4222eadc0e2f0ce42c6b93797a60ca4e2
Davi (Estudo) <daviamorim_sz@live.com>
2023-09-05 01:52:24 -0300

    feat-show_accounts

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,14 +7,17 @@ const dados_login = JSON.parse(localStorage.getItem('accounts'));
 form_login.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    dados_login.forEach(element => {
+    if (dados_login != null) {
+        dados_login.forEach(element => {
 
-        if (user_id.value === element.usuario && password.value === element.senha) {
-            window.location.href = "../home.html"
-            return
-        } else {
-            alert('deu b.o mermao');
-        }
-    });
+            if (user_id.value === element.usuario && password.value === element.senha) {
+                window.location.href = "../home.html"
+                return
+            } else {
+                alert('deu b.o mermao');
+            }
+        });
+    }
+
 
 })
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,30 @@
 const user = document.getElementById('user');
 const email = document.getElementById('user__email');
 const pass = document.getElementById('user__pass');
-const passConfirm = document.getElementById('user__pass__confirm');
 const form = document.getElementById('form');
+
+const div = document.getElementById('divAccounts');
 
 //const buscando a key 'accounts' no localStorage e fazendo a conversão do type caso tenha algo, caso não encontre nada ela cria um array vazio
 const dados = JSON.parse(localStorage.getItem('accounts')) || [];
+showAndUpdateElement(); //Verificando se há contas registradas, se sim, mostra na tela
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
     //Condicional validando se os campos estão vazios
     if (user.value.trim() === '' || email.value.trim() === '' || pass.value.trim() === '') {
         alert('Campos vazios, preencha corretamente.');
     } else {
         const newUser = user.value; //Obtendo o valor do user.value
-        const userExists = dados.find((element) => { //Buscando dentro do array 'dados' se já existe algum usuário com o valor do newUser
+        const userExists = dados.some((element) => { //Buscando dentro do array 'dados' se já existe algum usuário com o valor do newUser
             return element.usuario === newUser //Buscando exatamente o mesmo valor(.usuario) do newUser
         });
         if (userExists) {
             alert('Usuário já cadastrado!');
         } else {
             createAccount();
+            showAndUpdateElement();
         }
     }
 
@@ -48,4 +51,23 @@ function createAccount() {
     dados.push(newAccount);
     //Setando os itens da array dados no localStorage
     localStorage.setItem('accounts', JSON.stringify(dados));
+}
+
+function showAndUpdateElement(index) {
+    const element = dados[index];
+    dados.forEach(element => { //Para cada elemento
+        div.style.display = 'flex';
+        if (dados.length > 0) { //Verificando se há elementos dentro do array 'dados'
+            createElement(element);
+        }
+    });
+}
+
+function createElement(element) {
+    const newElement = document.createElement('span')
+    newElement.classList.add('style');
+
+    newElement.innerHTML = 'Usuário: ' + element.usuario + '<br>E-mail: ' + element.email + '<br>Senha: ' + element.senha;
+
+    div.appendChild(newElement);
 }
